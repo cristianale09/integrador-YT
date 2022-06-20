82e2b3a991731af5fcc4c4ffa6e56340ebc35052
Cristian Ale <cristianale09@outlook.com>
2022-06-20 14:30:47 -0300

    creacion de iconos social y banner

--- IntegradorArgentinaProgramaYT/src/app/app.module.ts
+++ IntegradorArgentinaProgramaYT/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { LofoAPComponent } from './Components/lofo-ap/lofo-ap.component';
+import { SocialComponent } from './Components/social/social.component';
+import { BannerComponent } from './Components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LofoAPComponent
+    LofoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
